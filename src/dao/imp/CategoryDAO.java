0fdd3965d0876e6f251499ff1fb2fe5554cfc1a7
JiangMin <opensource@jiangmin.me>
2012-08-21 10:51:22 +0800

    struts.xml文件名修正
    这些配置文件实在是太麻烦了……

--- src/dao/imp/CategoryDAO.java
+++ src/dao/imp/CategoryDAO.java
@@ -9,25 +9,23 @@ import domain.Category;
 
 public class CategoryDAO extends HibernateDaoSupport implements ICategoryDAO {
 
-	@Override
+	public CategoryDAO(){}
+	
 	public void addCategory(Category category) {
 		this.getHibernateTemplate().save(category);
 		
 	}
 
-	@Override
 	public void deleteCategory(Category category) {
 		this.getHibernateTemplate().delete(category);
 		
 	}
 
-	@Override
 	public void updateCategory(Category category) {
 		this.getHibernateTemplate().update(category);
 		
 	}
 
-	@Override
 	public Category getCategoryById(String id) {
 		return (Category)this.getHibernateTemplate().get(Category.class, id);
 		
@@ -36,13 +34,11 @@ public class CategoryDAO extends HibernateDaoSupport implements ICategoryDAO {
 	/**
 	 * 严格意义上讲，Category的Name并不唯一，此处返回取得的第一个值
 	 */
-	@Override
 	public Category getCategoryByName(String catName) {
 		String hsql = "FROM Category WHERE catName='"+catName+"'";
 		return (Category)this.getHibernateTemplate().find(hsql).get(0);
 	}
 
-	@Override
 	public List<Category> getAllCategorie() {
 		String hsql = "FROM Category";
 		return (List<Category>)this.getHibernateTemplate().find(hsql);
