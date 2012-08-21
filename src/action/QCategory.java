06466d4f31b1bb80a12c72025e9dccb0a07b66eb
JiangMin <opensource@jiangmin.me>
2012-08-21 16:40:04 +0800

    整体流程基本走通
    能够正确得到输出信息，下一步测试数据库连接

--- src/action/QCategory.java
+++ src/action/QCategory.java
@@ -1,5 +1,8 @@
 package action;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import service.ICategoryService;
 
 import com.opensymphony.xwork2.ActionSupport;
@@ -7,7 +10,16 @@ import com.opensymphony.xwork2.ActionSupport;
 public class QCategory extends ActionSupport {
 	private ICategoryService categoryService;
 	
+	private InputStream inputStream;
+	
 	public String execute() throws Exception {
+		inputStream = new ByteArrayInputStream("{name:jiangmin,age:24}".getBytes("UTF-8"));
+		System.out.println(inputStream);
+		return SUCCESS;
+	}
+	
+	public String  qCate() throws Exception {
+		inputStream = new ByteArrayInputStream("{name:jiangmin,age:24}".getBytes("UTF-8"));
 		return SUCCESS;
 	}
 
@@ -17,4 +29,13 @@ public class QCategory extends ActionSupport {
 	public void setCategoryService(ICategoryService categoryService) {
 		this.categoryService = categoryService;
 	}
+
+	/**
+	 * @return the inputStream
+	 */
+	public InputStream getInputStream() {
+		return inputStream;
+	}
+	
+	
 }
